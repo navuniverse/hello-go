c3c080b5631816e23385ec30e452e6575f09331b
Naveen Kumar <naveen.kumar@fabhotels.com>
2018-09-08 13:05:43 +0530

    add addition function

--- hello.go
+++ hello.go
@@ -8,4 +8,12 @@ import (
 func main() {
 	fmt.Printf("hello, world\n")
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+
+	fmt.Print("adding numbers : ")
+	fmt.Print(addNumbers(5, 6))
+
+}
+
+func addNumbers(a int, b int) int {
+	return a + b
 }
