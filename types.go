f0f40a63b14fb162211f4eb806a7051e97719189
Naveen Kumar <navuniverse@gmail.com>
2018-09-16 14:26:43 +0530

    Fixed types compilation issue

--- types.go
+++ types.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var x int = 1 // Integer Data Type
-	var y int     //  Integer Data Type
+	var x = 1 // Integer Data Type
+	var y int //  Integer Data Type
 	fmt.Println("x : ", x)
 	fmt.Println("y : ", y)
 
